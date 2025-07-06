e5ea77446c3d3521289e7802c5817c9c5d984622
mickeybot-79 <michaelperezvezoli@hotmail.com>
2025-07-06 14:48:18 -0300

    new commit

--- server.js
+++ server.js
@@ -14,28 +14,36 @@ app.get('/', (req, res) => {
 app.use(express.json({ limit: "50mb", extended: true }))
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 
-app.get('/transcribe', async (req, res) => {
+app.post('/transcribe', async (req, res) => {
+    //const {audio} = req.body
+
     const client = new AssemblyAI({
         apiKey: process.env.ASSEMBLYAI,
     })
 
-    const params = {
-        audio: "C:/Users/user/Downloads/recording.weba",
-        speech_model: "universal",
-    }
+    setTimeout(async () => {
+        const params = {
+            audio: "C:/Users/user/Downloads/recording.weba",
+            speech_model: "universal",
+        }
 
-    console.log('requesting transcript')
-    const transcript = await client.transcripts.transcribe(params)
+        // const params = {
+        //     audio: audio,
+        //     speech_model: "universal",
+        // }
 
-    if (transcript?.text) {
-        fs.unlink(params.audio,
-            err => {
-                if (err) console.log(err)
-            }
-        )
-        console.log('sending response')
-        res.send({ 'result': transcript.text })
-    }
+        const transcript = await client.transcripts.transcribe(params)
+
+        if (transcript?.text) {
+            fs.unlink(params.audio,
+                err => {
+                    if (err) console.log(err)
+                }
+            )
+            console.log('sending response')
+            res.send({ 'result': transcript.text })
+        }
+    }, 100)
     //res.send({ 'result': 'test response' })
 
 })
